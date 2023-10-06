0b6f137714758f36d5e74dd3de0a32f4560ae592
steven <ht37@rice.edu>
2023-10-06 15:16:18 -0500

    test redirection

--- SpringBoot-Backend/shortUrl/src/main/java/com/comp539/shorturl/controller/URLConversionController.java
+++ SpringBoot-Backend/shortUrl/src/main/java/com/comp539/shorturl/controller/URLConversionController.java
@@ -2,7 +2,9 @@ package com.comp539.shorturl.controller;
 
 import com.comp539.shorturl.dto.ToShortURLResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class URLConversionController {
@@ -10,5 +12,12 @@ public class URLConversionController {
     @GetMapping("to-shortURL")
     public ToShortURLResponse toShortURL(String longURL){
         return new ToShortURLResponse(longURL+"TestTest");
+    }
+
+    @RequestMapping("redirect-longURL")
+    public RedirectView redirectLongURL() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://www.yahoo.com");
+        return redirectView;
     }
 }
