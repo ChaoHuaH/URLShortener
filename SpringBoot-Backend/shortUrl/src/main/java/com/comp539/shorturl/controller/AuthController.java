aec44b83dcdc470032f8a5104ef40a63db611da3
Chelsea Chen <chelseachen@chelseas-air.dyndns.rice.edu>
2023-11-02 16:37:04 -0500

    test

---
+++ SpringBoot-Backend/shortUrl/src/main/java/com/comp539/shorturl/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.comp539.shorturl.controller;
+
+import com.comp539.shorturl.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestParam String email, @RequestParam String password, @RequestParam String name) {
+        try {
+            userService.registerUser(email, password, name);
+            return ResponseEntity.ok().body("User registered successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Registration failed: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestParam String email, @RequestParam String password) {
+        try {
+            String token = userService.authenticateUser(email, password);
+            return ResponseEntity.ok().body(token);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Login failed: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logoutUser(@RequestParam String token) {
+        userService.logoutUser(token);
+        return ResponseEntity.ok().body("Logged out successfully");
+    }
+}
---
+++ SpringBoot-Backend/shortUrl/src/main/java/com/comp539/shorturl/service/JwtTokenProvider.java
@@ -0,0 +1,27 @@
+package com.comp539.shorturl.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expirationMs}")
+    private int jwtExpirationMs;
+
+    public String createToken(Long userId, String email) {
+        return JWT.create()
+                .withSubject(String.valueOf(userId))
+                .withClaim("email", email)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .sign(Algorithm.HMAC512(jwtSecret));
+    }
+}
