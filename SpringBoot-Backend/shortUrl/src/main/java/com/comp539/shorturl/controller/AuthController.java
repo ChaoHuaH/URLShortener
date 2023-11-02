9c646590c661dce26c6f602c7231c1b350d03318
Chelsea Chen <chelseachen@chelseas-air.dyndns.rice.edu>
2023-11-02 17:26:35 -0500

    403 problem unfix

--- SpringBoot-Backend/shortUrl/src/main/java/com/comp539/shorturl/controller/AuthController.java
+++ SpringBoot-Backend/shortUrl/src/main/java/com/comp539/shorturl/controller/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = {"/api/auth"})
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
